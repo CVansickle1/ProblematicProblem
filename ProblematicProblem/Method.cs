de2699df30a16dce833bec57c373f07ce29ac3fb
Cody <Cojo341@gmail.com>
2022-08-29 12:50:58 -0400

    Fixed Errors

---
+++ ProblematicProblem/Method.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblematicProblem
+{
+    internal class Method
+    {
+        public static bool Participate(string ans)
+        {
+            while (true)
+            {
+                
+                if (ans != null && (ans == "y" || ans == "yes"))
+                {
+                    return true;
+
+                }
+                else if (ans != null && (ans == "n" || ans == "no"))
+                {
+                    return false;
+
+                }
+                else
+                {
+                    Console.WriteLine("Only y or n Allowed");
+                    ans = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
